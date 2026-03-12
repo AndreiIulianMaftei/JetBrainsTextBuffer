e15066fdfe117b88610d03d6ff7f23c593ce68bc
andrei <mafteiandreiiulian@gmial.com>
2026-03-12 21:10:24 +0200

    added tests for cell , and added visible atributes

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
             System.out.println("Cell foreground color: " + cell.getFg());
             System.out.println("Cell background color: " + cell.getBg());
             System.out.println("Cell is bold: " + cell.isBold());
-
             CellAttributes attrs = new CellAttributes(TerminalColor.GREEN, TerminalColor.DEFAULT, TextStyle.ITALIC);
             System.out.println("CellAttributes: " + attrs);
     }
